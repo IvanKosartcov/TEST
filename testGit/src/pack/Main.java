25df09b1e7eea77e377514ef86c4e9c3f0412828
Ivan_ItStep <ivan.kosar@yandex.ru>
2021-11-21 17:05:52 +0500

    sout 2

--- testGit/src/pack/Main.java
+++ testGit/src/pack/Main.java
@@ -3,6 +3,8 @@ package pack;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("hell");
+        System.out.println("hell");
+
     }
 }
